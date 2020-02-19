c914a3d913cac496000cddab3b91b2f68349316a
Abhinavgupta371 <abhinavgupta371@gmail.com>
2020-02-19 22:09:42 +0530

    tissot: remove aux blacklist & added extended support for it as well as replace codeaura snap camera with los snap camera Signed-off-by: MASTERGUY <murtipipo@gmail.com>

--- prop.mk
+++ prop.mk
@@ -7,8 +7,13 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-persist.camera.dual.camera=0
+vendor.camera.aux.packageblacklist=com.discord \
+persist.camera.HAL3.enabled=1 \
+vendor.camera.aux.packagelist=org.lineageos.snap,com.android.camera2,com.google.android.GoogleCamera \
+vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
+vendor.camera.hal1.packagelist=com.whatsapp \
+persist.camera.dual.camera=0 \
+persist.camera.eis.enable=1
 
 #Date Fixes
 PRODUCT_PROPERTY_OVERRIDES += \
