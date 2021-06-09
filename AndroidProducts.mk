ce602c3e622dc2bc6f02db76ff00d286629043e1
reharsh67 <reharshdeshpande@gmail.com>
2021-06-09 08:49:34 +0000

    Derpify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2018 The derpOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/derp_tissot.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_tissot-user \
+    derp_tissot-userdebug \
+    derp_tissot-eng \
